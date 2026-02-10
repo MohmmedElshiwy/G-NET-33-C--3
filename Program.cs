a8fab5aa70de494ae720f24885f15a590862eef9
MohmmedElshiwy <mohammed6elshiwy@gmail.com>
2026-02-10 13:31:50 +0300

    Problem 2 solutions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// #region problem 1
+
+//     /*1 - */
+
+// System.Console.WriteLine("Problem 1:");
+// double d = 9.99;
+// int x = (int)d;
+// Console.WriteLine(x);
+
+// // result = 9  
+// // because the decimal part is truncated when converting from double to int, leaving only the whole number part.
+// #endregion
+
+// #region problem 2
+//     /*2 - */
+
+
+// System.Console.WriteLine("Problem 2:");
+// int n = 5;
+// double d2 = n / 2.0;
+// Console.WriteLine(d2);
+
+// // result = 2.5
+
+
+// #endregion
